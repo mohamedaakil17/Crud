3410d8450cd11b0f571a2ed73f1928ac6e74be21
Ayoob1993 <mohamadayoob365@gmail.com>
2021-10-25 16:38:13 -0400

    layouts

---
+++ components/resources/views/welcome.blade.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Album example for Bootstrap</title>
+  </head>
+  <body>
+<style>
+.demo_heading{
+    color: #636C72;
+    padding-left: 25%;
+}
+
+.paragraph_1{
+   
+    width: 70%;
+    color: #636C72;
+    padding-left: 25%;
+}
+
+.paragraph_2{
+    color: #636C72;
+    padding-left: 85%;
+}
+
+.form-inline{
+    color: white;
+    padding-left: 25%;
+}
+
+.top_botton
+{
+    padding-right: 15%;
+
+}
+
+.para1{
+    padding-left: 25%;
+}
+
+
+</style>
+<div>
+    <nav class="navbar navbar-light bg-dark">
+     <form class="form-inline">
+            <font color="white">Album</font>
+        </form>    
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    </nav>
+</div>
+<div class="text-center">
+    </br></br></br>
+        <h1><center>Album Examples</center></h1>
+        <p><center>Something short and leading about the collection below--its</br>contents, the creator, etc. Make it short and sweet, but not too</br>short so folks don't simply skip over it entirely.</center></p>
+        <a class="btn btn-primary" href="register" role="button">Sign Up</a>
+        <button type="button" class="btn btn-outline-primary"><font color = "black">Secondary Action</font></button>
+</div>
+</br>
+<div class="text-left bg-light">
+   <!--  </br></br></br>
+        <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This is demo text</h1>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vehicula leo at nisi fringilla maximus.</br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vehicula leo at nisi fringilla maximus.</br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vehicula leo at nisi fringilla maximus.</p> -->
+    
+        <div class="alert alert-primary bg-light " role="alert" >
+            <h1 class="demo_heading">This is demo text</h1>
+            <p class="paragraph_1 text-justify">Lorem ipsum dolor sit amet, omnis impedit cum no, quod porro falli eos at. Ea etiam timeam luptatum duo, quis choro feugait quo ad. Pri eu meis regione labitur, nam ei esse aliquam, ex minim veritus per. Id pro tempor vocibus alienum. Prima fierent no his, latine splendide usu an, ad eos labore phaedrum voluptatibus. Diam tacimates vim et. An pri facer laudem possim, fugit suscipiantur est cu, quo ad elit iusto invenire</p>
+        </div>
+</div>
+<div>
+
+</div>
+
+
+
+   
+<div>
+    <!-- <p class="paragraph_1">Album example is © Bootstrap, but please download and customize it for yourself!</p> -->
+<!-- 
+    </br>New to Bootstrap?
+     <a href>Visit the homepage</a> 
+     or read our 
+     <a href>getting started guide.</a></a> or read our 
+     <a href>Back to Top</a> -->
+
+     <div class="d-flex bg-light align-items-center justify-content-between">
+         <div class="para1">
+
+         <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
+         </div>
+         <div class="top_botton">
+         <a href>Back to Top</a>
+         </div>
+     </div>
+    
+
+</div>
+
+
+ <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
